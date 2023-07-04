07cb47d060d774eee8ea97cf0a97feec3c4cf9dd
Karl <karl@karlsclipboard.com>
2023-07-03 22:22:38 -0400

    Added input to change file name

--- Java_Vocab_Project/File_Read_01.java
+++ Java_Vocab_Project/File_Read_01.java
@@ -1,11 +1,16 @@
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
+//import java.util.*;
 
 public class File_Read_01 {
   public static void main(String[] args) {
     try {
-      File myObj = new File("Biology_Terms_1_tab.txt");    //Input File Goes Here
+      Scanner sc=new Scanner(System.in);  //System.in is a standard input stream
+      System.out.print("Enter the file name: ");
+      String File_In_Name=sc.nextLine();
+      //File myObj = new File("Biology_Terms_1_tab.txt");    //Input File Goes Here
+      File myObj = new File(File_In_Name);    //Input File Goes Here
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
